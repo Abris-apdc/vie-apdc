bbfb9448087598260557d9ff2827e5d4f5f55060
rmr-henriques <rmr.henriques@campus.fct.unl.pt>
2021-05-15 15:22:24 +0100

    update ao codigo do antonio com requests, e com o css

--- src/RegisterUser.tsx
+++ src/RegisterUser.tsx
@@ -1,17 +1,18 @@
+import React from "react"
 import { ChangeEvent, ChangeEventHandler, useState} from "react"
 import './RegisterUser.css';
 
 function RegisterForm(){
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [username, setUserName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [confirmation, setConfirmation] = useState("")
-    const [date, setDate] = useState("")
-    const [year, setYear] = useState("")
-    const [month, setMonth] = useState("")
-    const [day, setDay] = useState("")
+    const [firstName1, setFirstName] = useState("")
+    const [lastName1, setLastName] = useState("")
+    const [username1, setUserName] = useState("")
+    const [email1, setEmail] = useState("")
+    const [password1,setPassword] = useState("")
+    const [confirmation1, setConfirmation] = useState("")
+
+    var day ="";
+    var month= ""; 
+    var year ="";
 
     function handleChangeFirstName(e:ChangeEvent<HTMLInputElement>){
         setFirstName(e.target.value)
@@ -38,48 +39,52 @@ function RegisterForm(){
     }
 
     function handleChangeDate(e:ChangeEvent<HTMLInputElement>){
-        setDay("1");
-        setMonth("1");
-        setYear("2000");
-       // if(e.target.valueAsDate != null){
-           // setDay(e.target.valueAsDate.getDay.toString)
-            //setMonth(e.target.valueAsDate.getMonth.toString)
-            //setYear(e.target.valueAsDate.getFullYear.toString)
-        //}
+        console.log(e.target.value);
+        var dateArr = e.target.value.split("-")
+        year=dateArr[0]
+        month=dateArr[1]
+        day=dateArr[2]
+        console.log(day,month,year)
     }
 
     function registerUser(){
         console.log("Registering User");
+        console.log(createJson());
+        console.log(firstName1);
         fetch("https://amazing-office-313314.appspot.com/rest/register",
             {method:"POST", 
-            headers:{ accept: "application/json"},
-            body: JSON.stringify({fisrtName:firstName, lastName:lastName, 
-                username:username, email:email, password:password, 
-                confirmation:confirmation, year:1999, month:1, day:29 })
+            headers:{ 'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json;charset=UTF-8'},
+            body: createJson()
             })
         .then(data=> {console.log(data)});
-     }
+    }
+
+    function createJson(){
+        return JSON.stringify({firstName:firstName1, lastName:lastName1,  
+            username:username1, email:email1, password:password1, 
+            confirmation:confirmation1, year:year, month:month, day:day})
+    }
    
     return <div className="container">
-        <form onSubmit={(e:any) => {e.preventDefault()}}>
-            <label>First Name:</label>
-            <input type={"text"} value={firstName} onChange={handleChangeFirstName}/>
-            <label>Last Name:</label>
-            <input type={"text"} value={lastName} onChange={handleChangeLastName}/>
-            <label>Username:</label> 
-            <input type={"text"} value={username} onChange={handleChangeUsername}/>
-            <label>Email:</label>
-            <input type={"text"} value={email} onChange={handleChangeEmail}/>
-            <label>Password:</label>
-            <input type={"password"} value={password} onChange={handleChangePassword}/>
-            <label>Confirm Password:</label>
-            <input type={"password"} value={confirmation} onChange={handleChangeConfirmation}/>
-            <label>Birthday:</label>
-            <input type={"date"} value={date} onChange={handleChangeDate} />
-        </form>
-        <button type="button" onClick={registerUser}>Register</button>
-    </div>
-
+    <form onSubmit={(e:any) => {e.preventDefault()}}>
+        <label>First Name:</label>
+        <input type={"text"} value={firstName1} onChange={handleChangeFirstName}/>
+        <label>Last Name:</label>
+        <input type={"text"} value={lastName1} onChange={handleChangeLastName}/>
+        <label>Username:</label> 
+        <input type={"text"} value={username1} onChange={handleChangeUsername}/>
+        <label>Email:</label>
+        <input type={"text"} value={email1} onChange={handleChangeEmail}/>
+        <label>Password:</label>
+        <input type={"password"} value={password1} onChange={handleChangePassword}/>
+        <label>Confirm Password:</label>
+        <input type={"password"} value={confirmation1} onChange={handleChangeConfirmation}/>
+        <label>Birthday:</label>
+        <input type={"date"} onChange={handleChangeDate} />
+    </form>
+    <button type="button" onClick={registerUser}>Register</button>
+</div>
 
 }
 
