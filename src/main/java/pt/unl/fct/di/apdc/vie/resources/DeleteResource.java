f0cfd6aeab0e0c7fe234b77006483c0a6b1249fc
unknown <il.rocha@campus.fct.unl.pt>
2021-07-25 11:22:54 +0100

    delete updated

--- src/main/java/pt/unl/fct/di/apdc/vie/resources/DeleteResource.java
+++ src/main/java/pt/unl/fct/di/apdc/vie/resources/DeleteResource.java
@@ -1,6 +1,8 @@
 package pt.unl.fct.di.apdc.vie.resources;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
@@ -17,7 +19,9 @@ import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.ListValue;
 import com.google.cloud.datastore.Transaction;
+import com.google.cloud.datastore.Value;
 import com.google.gson.Gson;
 
 import pt.unl.fct.di.apdc.vie.util.DeleteData;
@@ -90,7 +94,68 @@ public class DeleteResource {
 			}
 			
 			if (token.getString("token_username").equals(data.getUsername())) {
-				//ele tenta remover-se a si proprio
+				//CLEAN FOLLOWING
+				List<Value<String>> following = account.getList("account_following_list");
+				for(int i = 0; i < following.size(); i++) {
+					Key followingKey = datastore.newKeyFactory().setKind("Account").newKey(following.get(i).get());
+					Entity followingAcc = txn.get(followingKey);
+					List<Value<String>> following2 = followingAcc.getList("account_followers_list");
+					long nFollowers = followingAcc.getLong("account_followers");
+					List<Value<String>> following2New = new ArrayList<>();
+					for(int j = 0; j < following2.size(); j++) {
+						if(!following2.get(j).get().equals(data.getUsername())) {
+							following2New.add(following2.get(j));
+						}
+					}
+					nFollowers --;
+					followingAcc = Entity.newBuilder(followingAcc)
+							.set("account_followers_list", ListValue.of(following2New))
+							.set("account_followers", nFollowers)
+							.build();
+					txn.update(followingAcc);
+						
+				}
+				//CLEAN FOLLOWERS
+				List<Value<String>> followers = account.getList("account_followers_list");
+				for(int i = 0; i < followers.size(); i++) {
+					Key followerKey = datastore.newKeyFactory().setKind("Account").newKey(followers.get(i).get());
+					Entity followerAcc = txn.get(followerKey);
+					List<Value<String>> follower = followerAcc.getList("account_following_list");
+					long nFollowing = followerAcc.getLong("account_following");
+					List<Value<String>> followerNew = new ArrayList<>();
+					for(int j = 0; j < follower.size(); j++) {
+						if(!follower.get(j).get().equals(data.getUsername())) {
+							followerNew.add(follower.get(j));
+						}
+					}
+					nFollowing --;
+					followerAcc = Entity.newBuilder(followerAcc)
+							.set("account_following_list", ListValue.of(followerNew))
+							.set("account_following", nFollowing)
+							.build();
+					txn.update(followerAcc);
+						
+				}
+				
+				//CLEAN EVENTS JOINED
+				List<Value<String>> events = account.getList("account_events_list");
+				for(int i = 0; i < events.size(); i++) {
+					Key eventKey = datastore.newKeyFactory().setKind("Event").newKey(events.get(i).get());
+					Entity event = txn.get(eventKey);
+					List<Value<String>> participants = event.getList("event_participants_list");
+					List<Value<String>> participantsNew = new ArrayList<>();
+					for(int j = 0; j < participants.size(); j++) {
+						if(!participants.get(j).get().equals(data.getUsername())) {
+							participantsNew.add(participants.get(j));
+						}
+					}
+					event = Entity.newBuilder(event)
+							.set("event_participants_list", ListValue.of(participantsNew))
+							.build();
+					txn.update(event);
+						
+				}
+				
 				txn.delete(tokenKey);
 				txn.delete(accountKey);
 				txn.commit();
@@ -114,5 +179,13 @@ public class DeleteResource {
 				return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 			}
 		}
+	}
+	
+	private void removeFollows() {
+		
+	}
+	
+	private void removeEvents() {
+		
 	}
 }
